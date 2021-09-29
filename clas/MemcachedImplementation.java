809a572b03e9e112aed0aa1bd6a7fe30a1b76d20
VladFedorenko123 <74425481+VladFedorenko123@users.noreply.github.com>
2021-09-29 14:01:54 +0300

    @VladFedorenko123 SpringData + Lombok+Logback + Memcached

--- clas/MemcachedImplementation.java
+++ clas/MemcachedImplementation.java
@@ -1,21 +1,23 @@
 package com.srccode.clas;
 
-import com.srccode.Application;
 import com.srccode.interfaces.Memcached;
 import net.spy.memcached.MemcachedClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
+@Service
 public class MemcachedImplementation implements Memcached {
     private static final Logger logger = LoggerFactory.getLogger(MemcachedImplementation.class);
 
+    @Override
     public void mCache(String key, String value) {
         try {
-            MemcachedClient memcachedClient = new MemcachedClient(new InetSocketAddress("localhost", 12345));
+            ApplicationPropertiesSingleton  applicationProperties = new ApplicationPropertiesSingleton();
+            MemcachedClient memcachedClient = new MemcachedClient(new InetSocketAddress(applicationProperties.ip, Integer.parseInt(applicationProperties.port)));
             logger.info("Connection to server successfully");
             logger.info("set status " + memcachedClient.set(key, 60, value).isDone());
             logger.info("add status " + memcachedClient.add(key, 60, value).isDone());
